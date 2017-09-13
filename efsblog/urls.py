ef388e6e72c6359875ea5c57011d65c325ad40f2
mdondeti <vdondeti@unomaha.edu>
2017-09-13 16:25:45 -0500

    Code Added to urls file.

--- efsblog/urls.py
+++ efsblog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'', include('portfolio.urls', namespace='portfolio')),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/profile/$', views.home, name='home'),
 ]
