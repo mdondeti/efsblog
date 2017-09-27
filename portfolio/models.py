3cab602e07b28e6210ae76a6ef70c69d18261e76
mdondeti <vdondeti@unomaha.edu>
2017-09-27 00:27:08 -0500

    Assign1P3 without Enhancements.

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from yahoo_finance import Share
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
@@ -37,6 +38,7 @@ class Investment(models.Model):
     recent_value = models.DecimalField(max_digits=10, decimal_places=2)
     recent_date = models.DateField(default=timezone.now, blank=True, null=True)
 
+
     def created(self):
         self.acquired_date = timezone.now()
         self.save()
@@ -50,6 +52,7 @@ class Investment(models.Model):
 
     def results_by_investment(self):
         return self.recent_value - self.acquired_value
+
 
 
 class Stock(models.Model):
@@ -69,4 +72,24 @@ class Stock(models.Model):
 
     def initial_stock_value(self):
         return self.shares * self.purchase_price
+
+    def current_stock_price(self):
+        symbol_f = self.symbol
+        data = Share(symbol_f)
+        share_value = (data.get_open())
+        return (share_value)
+
+    def current_stock_value(self):
+        symbol_f = self.symbol
+        data = Share(symbol_f)
+        share_value = (data.get_open())
+        if share_value is not None:
+         share_value = float(share_value)
+         return (share_value) * float(self.shares)
+
+
+
+
+
+
 
